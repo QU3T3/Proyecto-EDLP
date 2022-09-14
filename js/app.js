9bf44a4a05d9f2381ea95c69cd5909d959bc4a87
Franco <fnosettoecheverria@hotmail.com>
2022-09-14 19:58:03 -0300

    Creando carrito de compras

--- js/app.js
+++ js/app.js
@@ -1,55 +1,74 @@
-function chequeoVentana(ventana) {
+const productos = [
+    { nombre: "camiseta2006", precio: 8000 },
+    { nombre: "camisetaSuplente2006", precio: 6000 },
+    { nombre: "camiseta2009", precio: 10000 },
+    { nombre: "camisetaSuplente2009", precio: 9000 },
+    { nombre: "camiseta2010", precio: 7000 },
+    { nombre: "camisetaSuplente2010", precio: 6500 },
+];
 
-    let puntos = Number(prompt(`¿Que rango de puntos te encontras?:
+let carrito = []
 
-    1. 0 a 999
+let seleccion = prompt("Hola! ¿Desea comprar algún producto?")
 
-    2. 999 a 1200
+while(seleccion != "si" && seleccion != "no"){
+    alert("Por favor, ingrese si o no")
+    seleccion = prompt("Hola! ¿Desea comprar algún producto?")
+}
 
-    3. 1200 a 1555`));
+if(seleccion === "si"){
+    alert("A continuación nuestra lista de productos!")
+    let todosLosProdcutos = productos.map((producto) =>  producto.nombre + " " + "$" + producto.precio);
+    alert(todosLosProdcutos.join(" - "))
+} else if (seleccion == "no"){
+    alert("¡Gracias por venir, hasta pronto!")
+}
 
-    switch (puntos) {
+while(seleccion != "no"){
+    let producto = prompt("Agrega un producto a tu carrito")
+    let precio = 0
 
-        case 1:
-
-            alert(`Tu día de canje es el lunes`)
-
-            break;
-
-        case 2:
-
-            alert(`Tu día de canje es el martes`)
-
-            break;
-
-           case 3:   
-
-            alert(`Tu día de canje es el lunes`)
-
-            break;
-
-        default:
-
-            alert(`ingrese correctamente su rango`)
-
-            break;
-
+    if(producto == "camiseta2006" || producto =="camisetaSuplente2006" || producto == "camiseta2009" || producto =="camisetaSuplente2009" ||producto == "camiseta2010" || producto =="camisetaSuplente2010" ){
+        switch(producto){
+            case "camiseta2006":
+                precio = 8000
+                break;
+            case "camisetaSuplente2006":
+                precio = 6000
+                break;
+            case "camiseta2009":
+                precio = 10000
+                break;
+            case "camisetaSuplente2009":
+                precio = 9000
+                break;
+            case "camiseta2010":
+                precio = 7000
+                break;
+            case "camisetaSuplente2010":
+                precio = 6500
+                break;
+            default:
+                break;
         }
+    let unidades = parseInt(prompt("¿Cuantas unidades quiere llevar?"))
 
-    ventana=puntos;
+    carrito.push({producto, unidades, precio})
+    } else{
+        alert("No contamos con ese producto.")
+    }
 
-   return alert(`Te encuentras en el rango de puntos:`+ ventana +' '+ `sigue sumando para tener mejor posicionamiento` )  
+    seleccion = prompt("¿Desea seguir comprando?")
 
+    while(seleccion === "no"){
+        alert("¡Gracias por su compra!")
+        carrito.forEach((carritoFinal) => {
+            console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+    break;
+    }
 }
 
-let socio = prompt(`¿Sos socio del club y queres saber tu ranking?`);
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0)
 
-while (socio === "si") {
-
-    chequeoVentana();
-
-    socio = prompt("Quieres consultar de nuevo?");
-
-}
-
-alert(`adios!`)
+console.log(`El total a pagar es: ${total}`)
